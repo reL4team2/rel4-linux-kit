57d7d83439693e063808ddaf53510886a99b262e
yfblock <321353225@qq.com>
2025-06-01 23:25:00 +0800

    feat: remove useless deps

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -17,4 +17,6 @@ pub mod slot;
 pub use common_macros::{generate_ipc_send, ipc_trait, ipc_trait_impl};
 pub use obj_allocator::*;
 
+pub use sel4_logging::{Logger, LoggerBuilder};
+
 // FIXME: Make this variable more generic.
