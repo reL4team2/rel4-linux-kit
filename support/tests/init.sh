066cb43a1f2d27d4feecd474f51ccb4d4747473a
yfblock <321353225@qq.com>
2025-08-25 02:49:04 +0800

    feat: add basic for newer gcc

--- support/tests/init.sh
+++ support/tests/init.sh
@@ -1,37 +1,37 @@
 echo Hello World!
 
-# ./brk
-# ./chdir
-# ./clone
-# ./close
-# ./dup
-# ./dup2
-# ./execve
-# ./exit
-# ./fork
-# ./fstat
-# ./getcwd
-# ./getdents
-# ./getpid
-# ./getppid
-# ./gettimeofday
-# ./mkdir_
-# ./mmap
-# ./mount
-# ./munmap
-# ./open
-# ./openat
-# ./pipe
-# ./read
-# ./sleep
-# ./test_echo
-# ./umount
-# ./uname
-# ./unlink
-# ./wait
-# ./waitpid
-# ./write
-# ./yield
+./brk
+./chdir
+./clone
+./close
+./dup
+./dup2
+./execve
+./exit
+./fork
+./fstat
+./getcwd
+./getdents
+./getpid
+./getppid
+./gettimeofday
+./mkdir_
+./mmap
+./mount
+./munmap
+./open
+./openat
+./pipe
+./read
+./sleep
+./test_echo
+./umount
+./uname
+./unlink
+./wait
+./waitpid
+./write
+./yield
 
 ./busybox ash run-static.sh
 ./busybox sh busybox_testcode.sh
